a0777d884580b7f02ee3f8c2305aebb7f7db2e1a
ganesh070723 <bbabu8342@gmail.com>
2023-01-18 20:33:43 +0530

    last

--- gpt/settings.py
+++ gpt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9x)vyb194hx9cml3427da*@lqf9c+unspbk)tds=-a+@w32!ac'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
